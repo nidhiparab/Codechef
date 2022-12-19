cac6efc3360d5785b7b3c2f5ab6da592e6eff3e6
Nidhi Parab <nidhiparabmail@gmail.com>
2022-12-19 22:08:46 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+def percent():
+    stock:{
+        date:
+        price:
+    }
+    today.price + 0.05*today.price == prev.price or today.price - 0.05*today.price == prev.price
+    return prev
+
+print(percent)
